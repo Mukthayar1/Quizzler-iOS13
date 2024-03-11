41fb6a6acd9946b55a0d27658285d9566530e955
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2024-03-11 17:29:07 +0500

    working on

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -19,22 +19,33 @@ class ViewController: UIViewController {
    [ "4 + 5 = 10 ?","False"],
    ["4 * 5 = 20 ?","True"],
    [ "4 - 5 = 0 ?","True"],
-    ]
+    ];
     
-    var QuestionNo = 0
+    var QuestionNo = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         QuestionLabel.text = quizzArray[QuestionNo][0]
     }
+    
+    func showAlert() {
+        let alertController = UIAlertController(title: "WRONG ANSWER", message: "YOU SELECTED WRONG ANSWER", preferredStyle: .alert)
+
+            let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                print("OK button pressed")
+            }
+            alertController.addAction(okAction)
+
+            present(alertController, animated: true, completion: nil)
+        }
 
     @IBAction func ButtonPress(_ sender: UIButton) {
         
         let UserAnswer = sender.currentTitle;
         let CorrectAnswer = quizzArray[QuestionNo][1];
         
-        print(UserAnswer!)
-        print(CorrectAnswer)
+        print("USER ANSWER ====> ",UserAnswer!)
+        print("CORRECT ANSWER ====>",CorrectAnswer)
         
         if UserAnswer == CorrectAnswer {
             if QuestionNo + 1 < quizzArray.count {
@@ -42,8 +53,10 @@ class ViewController: UIViewController {
                 QuestionNo += 1
             }else{
                 QuestionLabel.text = quizzArray[0][0]
-                QuestionNo = 1
+                QuestionNo = 0
             }
+        }else {
+            showAlert();
         }
       
 
